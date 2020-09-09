ce99214fab1645b2e5b7509b66740617a391fdad
unknown <bagasnuralim@gmail.com>
2020-09-09 16:46:21 +0700

    second

---
+++ application/views/common/header.php
@@ -0,0 +1,41 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!--
+=========================================================
+* Argon Design System - v1.2.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/argon-design-system
+* Copyright 2020 Creative Tim (http://www.creative-tim.com)
+
+Coded by www.creative-tim.com
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software. -->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="apple-touch-icon" sizes="76x76" href="./assets/img/apple-icon.png">
+  <link rel="icon" type="image/png" href="<?= base_url('assets/img/brand/logo-inaba.png'); ?>">
+  <title>
+    Direktorat Kemahasiswaan STIE INABA
+  </title>
+  <!--     Fonts and icons     -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
+  <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
+  <!-- Nucleo Icons -->
+  <link href="<?php echo base_url('assets/css/nucleo-icons.css'); ?>" rel="stylesheet" />
+  <link href="<?php echo base_url('assets/css/nucleo-svg.css'); ?>" rel="stylesheet" />
+  <!-- Font Awesome Icons -->
+  <link href="<?php echo base_url('assets/css/font-awesome.css'); ?>" rel="stylesheet" />
+  <link href="<?php echo base_url('assets/css/nucleo-svg.css'); ?>" rel="stylesheet" />
+  <!-- CSS Files -->
+  <link href="<?php echo base_url('assets/css/argon-design-system.css?v=1.2.0'); ?>" rel="stylesheet" />
+</head>
+
+<body class="index-page">
